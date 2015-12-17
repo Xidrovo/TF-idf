54d1fc7f7036fd182f59c0ac11cd877d9af5e598
jguilindro <julio.guilindro@gmail.com>
2015-12-17 01:52:34 -0500

    TF-IDF agregado :new_moon_with_face: :new_moon_with_face: :new_moon_with_face:

--- python/steeeming.py
+++ python/steeeming.py
@@ -1,7 +1,10 @@
 __author__ = 'CltControl'
-import shutil
 import os
-#import NLTK
+import shutil
+import math
+from textblob import TextBlob as tb
+import nltk
+nltk.download()
 
 op=1 # variable para switch
 
@@ -47,6 +50,7 @@ switch = {
         }
 
 #Opciones del menu principal
+
 while (op!='0'):
     op=input("""
     Selecione una opcion:
@@ -54,12 +58,12 @@ while (op!='0'):
     2.-Eliminar palabras
     3.-Ingresar Palabras de Busqueda
     4.- Mostrar Estadisticas
-    0.-Salir
+   0.-Salir
 
     """)
     try:
         switch[op]() #selecciona opcion del switch
-    except KeyError:
+   except KeyError:
         default()  #si la opcion ingresada no es valida el dicionario devuelve una exception la que es manejada como un default
 
 
@@ -69,3 +73,49 @@ while (op!='0'):
 #print (a)
 # f.close()
 
+
+#Ejemplo de como usar la libreria de tf-idf
+# def tf(word, blob):
+#     return blob.words.count(word) / len(blob.words)
+#
+# def n_containing(word, bloblist):
+#     return sum(1 for blob in bloblist if word in blob)
+#
+# def idf(word, bloblist):
+#     return math.log(len(bloblist) / (1 + n_containing(word, bloblist)))
+#
+# def tfidf(word, blob, bloblist):
+#     return tf(word, blob) * idf(word, bloblist)
+#
+# document1 = tb("""Python is a 2000 made-for-TV horror movie directed by Richard
+# Clabaugh. The film features several cult favorite actors, including William
+# Zabka of The Karate Kid fame, Wil Wheaton, Casper Van Dien, Jenny McCarthy,
+# Keith Coogan, Robert Englund (best known for his role as Freddy Krueger in the
+# A Nightmare on Elm Street series of films), Dana Barron, David Bowe, and Sean
+# Whalen. The film concerns a genetically engineered snake, a python, that
+# escapes and unleashes itself on a small town. It includes the classic final
+# girl scenario evident in films like Friday the 13th. It was filmed in Los Angeles,
+#  California and Malibu, California. Python was followed by two sequels: Python
+#  II (2002) and Boa vs. Python (2004), both also made-for-TV films.""")
+#
+# document2 = tb("""Python, from the Greek word (πύθων/πύθωνας), is a genus of
+# nonvenomous pythons[2] found in Africa and Asia. Currently, 7 species are
+# recognised.[2] A member of this genus, P. reticulatus, is among the longest
+# snakes known.""")
+#
+# document3 = tb("""The Colt Python is a .357 Magnum caliber revolver formerly
+# manufactured by Colt's Manufacturing Company of Hartford, Connecticut.
+# It is sometimes referred to as a "Combat Magnum".[1] It was first introduced
+# in 1955, the same year as Smith & Wesson's M29 .44 Magnum. The now discontinued
+# Colt Python targeted the premium revolver market segment. Some firearm
+# collectors and writers such as Jeff Cooper, Ian V. Hogg, Chuck Hawks, Leroy
+# Thompson, Renee Smeets and Martin Dougherty have described the Python as the
+# finest production revolver ever made.""")
+#
+# bloblist = [document1, document2, document3]
+# for i, blob in enumerate(bloblist):
+#     print("Top words in document {}".format(i + 1))
+#     scores = {word: tfidf(word, blob, bloblist) for word in blob.words}
+#     sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+#     for word, score in sorted_words[:3]:
+#         print("Word: {}, TF-IDF: {}".format(word, round(score, 5)))
