83f1068a9046473cfa341824b5d272513f6f65a4
jguilindro <julio.guilindro@gmail.com>
2015-12-17 19:27:34 -0500

    Agregada Librería Misteriosa C :alien:

--- ProyectoLenguajes/nbproject/Makefile-variables.mk
+++ ProyectoLenguajes/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_PLATFORM_Debug=MinGW-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/MinGW-Windows
 CND_ARTIFACT_NAME_Debug=proyectolenguajes
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/proyectolenguajes
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/MinGW-Windows/proyectolenguajes
+CND_PACKAGE_DIR_Debug=dist/Debug/MinGW-Windows/package
 CND_PACKAGE_NAME_Debug=proyectolenguajes.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/proyectolenguajes.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/MinGW-Windows/package/proyectolenguajes.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_PLATFORM_Release=MinGW-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/MinGW-Windows
 CND_ARTIFACT_NAME_Release=proyectolenguajes
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/proyectolenguajes
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/MinGW-Windows/proyectolenguajes
+CND_PACKAGE_DIR_Release=dist/Release/MinGW-Windows/package
 CND_PACKAGE_NAME_Release=proyectolenguajes.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/proyectolenguajes.tar
+CND_PACKAGE_PATH_Release=dist/Release/MinGW-Windows/package/proyectolenguajes.tar
 #
 # include compiler specific variables
 #
